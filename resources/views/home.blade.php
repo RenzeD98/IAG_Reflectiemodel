c8c4ac42de8c06ffc4f21ae386af38e45c998a25
RenzeD98 <renze.d98@gmail.com>
2017-04-19 14:01:51 +0200

    setup for reflection page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,42 +4,42 @@
 <div class="container home">
     <div class="panel panel-default">
         <div class="panel-heading">
-              Welkom {{ Auth::user()->firstname }} {{ Auth::user()->lastname }} 
+              Welkom {{ Auth::user()->firstname }} {{ Auth::user()->lastname }}
         </div>
         <br />
 
         <div class="row row-centered">
             <a href="{{ url('feedback') }}">
-                <div class="col-xs-4 col-centered column">  
+                <div class="col-xs-4 col-centered column">
                      <i class="fa fa-forward fa-5x" aria-hidden="true"></i>
                      <span class="subtitle">Feedback</span>
                 </div>
             </a>
 
 
-            <a href="{{ url('feedback') }}">
-                <div class="col-xs-4 col-centered col-xs-offset-1 column">  
+            <a href="{{ url('reflection') }}">
+                <div class="col-xs-4 col-centered col-xs-offset-1 column">
                      <i class="fa fa-repeat fa-5x" aria-hidden="true"></i>
-                     <span class="subtitle">Reflecteren</span>
+                     <span class="subtitle">Reflectie maken</span>
                 </div>
             </a>
         </div>
 
         <div class="row row-centered">
             <a href="{{ url('feedback') }}">
-                <div class="col-xs-4 col-centered column"> 
+                <div class="col-xs-4 col-centered column">
                     <i class="fa fa-refresh fa-5x" aria-hidden="true"></i>
                     <span class="subtitle">IAG Reflectiemodel</span>
                 </div>
             </a>
 
             <a href="{{ url('profiel') }}">
-                <div class="col-xs-4 col-centered col-xs-offset-1 column">  
+                <div class="col-xs-4 col-centered col-xs-offset-1 column">
                     <i class="fa fa-user fa-5x" aria-hidden="true"></i>
                     <span class="subtitle">Profiel</span>
                 </div>
-            </a>     
+            </a>
       </div>
-    
+
 </div>
 @endsection
