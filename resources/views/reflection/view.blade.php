b6e4cb4ef2e7c2a21af3284337a4b04dd6d76337
Jeffrey Pieksma <jefreypieksma@hotmail.com>
2017-05-02 14:12:44 +0200

    Reflectie, update, list, view
    File names, controllers en views updates/creations!

---
+++ resources/views/reflection/view.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.mobile')
+
+@section('content')
+<div class="container reflection">
+  <div class="navigation">
+    <a href="/reflecties" class="give-feedback">Terug</a>
+    <a href="/reflectie/{{ $reflection->id }}/edit" class="give-feedback">Wijzigen</a>
+    <a href="/reflectie/{{ $reflection->id }}/delete" class="give-feedback">Verwijderen</a>
+  </div>
+
+    <div class="row">
+      <div class="col-md-12 head">
+        <img class="img-circle" src="http://placehold.it/50x50" alt="">
+        <p>{{ $reflection->title }}</p>
+        <p class="time"></p>
+      </div>
+
+      <div class="col-md-12 message">
+        <p>{{ $reflection->message }}</p>
+      </div>
+
+        
+      <div class="col-md-6">
+        <span class="tag">{{ $reflection->title }}</span>
+      </div>
+       
+    </div>
+    
+</div>
+@endsection
