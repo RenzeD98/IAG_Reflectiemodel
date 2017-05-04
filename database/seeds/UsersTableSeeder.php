0b0d29ff8e7cc7e15e588bb05af4c5555548ab00
MrPAttema <patrickattema@home.nl>
2017-05-04 15:08:36 +0200

    DB_seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->insert([
+          'firstname' => 'admin',
+          'lastname' => 'test',
+          'email' => 'admin@admin.nl',
+          'password' => bcrypt('admin'),
+      ]);
+    }
+}
