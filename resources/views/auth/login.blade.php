342093dfb0f756fa2646d285ae126b856ebf4ff4
Jeffrey Pieksma <jefreypieksma@hotmail.com>
2017-05-09 12:15:07 +0200

    Login html/css
    Fix for desktop

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,14 +8,12 @@
                 <div class="header">
                     <img src="icons/ProfileIcon.svg " alt="Reflectie icoon">
                 </div>
-
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Adres</label>
-
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
@@ -54,12 +52,13 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
+                            <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">Inloggen</button>
                             </div>
                         </div>
+                        
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
+                            <div class="col-md-6 col-md-offset-4">
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     Wachtwoord Vergeten?
                                 </a>
