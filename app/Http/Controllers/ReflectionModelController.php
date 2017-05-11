36fd18b355c53f97bff26f079340a3124219fb44
RenzeD98 <renze.d98@gmail.com>
2017-05-11 11:51:44 +0200

    asdf

--- app/Http/Controllers/ReflectionModelController.php
+++ app/Http/Controllers/ReflectionModelController.php
@@ -12,42 +12,42 @@ class ReflectionModelController extends Controller
     }
 
     //individual radar return view
-    public function radar(Radar $radar)
+
+    // public function radar(Radar $radar)
+    // {
+    //   return view('reflectionmodel.template', compact('radar'));
+    // }
+
+    public function custom()
     {
-
-      return view('reflectionmodel.template', compact('radar'));
+      return view('reflectionmodel.custom');
     }
-
-    // public function custom()
-    // {
-    //   return view('reflectionmodel.custom');
-    // }
-    // public function feedback()
-    // {
-    //   return view('reflectionmodel.feedback');
-    // }
-    // public function focus()
-    // {
-    //   return view('reflectionmodel.focus');
-    // }
-    // public function methodology()
-    // {
-    //   return view('reflectionmodel.methodology');
-    // }
-    // public function model()
-    // {
-    //   return view('reflectionmodel.model');
-    // }
-    // public function strategy()
-    // {
-    //   return view('reflectionmodel.strategy');
-    // }
-    // public function theory()
-    // {
-    //   return view('reflectionmodel.theory');
-    // }
-    // public function transparent()
-    // {
-    //   return view('reflectionmodel.transparent');
-    // }
+    public function feedback()
+    {
+      return view('reflectionmodel.feedback');
+    }
+    public function focus()
+    {
+      return view('reflectionmodel.focus');
+    }
+    public function methodology()
+    {
+      return view('reflectionmodel.methodology');
+    }
+    public function model()
+    {
+      return view('reflectionmodel.model');
+    }
+    public function strategy()
+    {
+      return view('reflectionmodel.strategy');
+    }
+    public function theory()
+    {
+      return view('reflectionmodel.theory');
+    }
+    public function transparent()
+    {
+      return view('reflectionmodel.transparent');
+    }
 }
--- resources/views/reflectionmodel/theory.blade.php
+++ resources/views/reflectionmodel/theory.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="container reflectiemodel">
-    <div class="">
-      <h1></h1>
+    <div class="reflectioncontainer">
+      <h1>theorie</h1>
     </div>
 </div>
 @endsection
