73c4466093c3faa4ff59dd2b0b002e8a983db14b
MrPAttema <patrickattema@home.nl>
2017-05-17 11:10:23 +0200

    Update_Laravel_Taal

---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Het wachtoowrd moet minimaal 6 tekens bevatten en overeen komen.',
+    'reset' => 'Uw wachtwoord is gereset!',
+    'sent' => 'Wij hebben u een e-mail gestuurd met reset link!',
+    'token' => 'Deze wachtwoord reset link is ongeldig.',
+    'user' => 'Wij kunnen geen gebruiker vinden met dit e-mail adres',
+];
