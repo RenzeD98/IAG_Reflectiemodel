06f8aeb5516512c0f84add0e93a28ebc0b519107
RenzeD98 <renze.d98@gmail.com>
2017-06-08 13:44:35 +0200

    fix posities

--- resources/views/reflectionmodel/index.blade.php
+++ resources/views/reflectionmodel/index.blade.php
@@ -45,7 +45,7 @@
       </div>
 
       <div class="radarinfo">
-        <h2><i class="fa fa-info-circle" aria-hidden="true"></i> Klik op de radar of positie voor meer informatie</h2>
+        <h2><i class="fa fa-info-circle" aria-hidden="true"></i> Klik op een radar of positie voor meer informatie</h2>
       </div>
 
     </div>
