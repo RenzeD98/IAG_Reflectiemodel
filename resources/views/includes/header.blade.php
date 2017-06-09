f6b386d013399928dc6fdf93a12012866d11471e
MrPAttema <patrickattema@home.nl>
2017-06-09 14:50:26 +0200

    Update_CSS_Header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,26 +1,15 @@
 <div class="container">
-    <div class="navbar-header">
-
-        <!-- Collapsed Hamburger -->
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-            <span class="sr-only">Toggle Navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-
         <!-- Branding Image -->
         <a class="navbar-brand" href="{{ url('/') }}">
             <img src="{{ URL::asset('images/IAG direct liggend outline-01.svg') }}" alt="" class="header-logo">
         </a>
-    </div>
 
-    <div class="collapse navbar-collapse" id="app-navbar-collapse">
+    <div class="collapse navbar-collapse">
         <!-- Left Side Of Navbar -->
-        <ul class="nav navbar-nav">
-          @if (Auth::guest())
-          <!-- show nothing -->
-          @else
+        @if (Auth::guest())
+        <!-- show nothing -->
+        @else
+        <ul class="nav navbar-nav navbar-right">
           <li><a href="/reflecties">Mijn reflectie</a></li>
           <li><a href="/feedback">Feedback geven</a></li>
           <li><a href="/profile">Profiel</a></li>
@@ -44,25 +33,6 @@
                   {{ csrf_field() }}
               </form>
             </li>
-
-                <!-- <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                        {{ Auth::user()->firstname }} {{ Auth::user()->lastname }} <span class="caret"></span>
-                    </a>
-
-                    <ul class="dropdown-menu" role="menu">
-                        <li>
-                            <a href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                                Uitloggen
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                {{ csrf_field() }}
-                            </form>
-                        </li>
-                    </ul>
-                </li> -->
             @endif
         </ul>
     </div>
